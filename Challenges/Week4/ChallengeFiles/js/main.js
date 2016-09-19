8c80565a9544b810d56431648cca7fa741144bfb
Nesto <bumbolio@gmail.com>
2016-09-18 22:12:36 -0400

    Completed assignment using jQuery

--- Challenges/Week4/ChallengeFiles/js/main.js
+++ Challenges/Week4/ChallengeFiles/js/main.js
@@ -6,11 +6,27 @@ function openBlock (id) {
         blocks[y].style.display = "none";
     }
     block.style.display = "block";
-    
+    $('#menu li').removeClass('active');
+
+    $('#' + 'menu_item_' + id).parent().addClass('active');
+
     // TODO: remove the "active" class from all of the li elements inside the menu
     // TODO: add the "active" class to the li element that contains the link that was clicked
 
 }
+
+$('#menu a').click(function(e){
+    e.preventDefault();
+    openBlock(this.id.replace('menu_item_', ''));
+});
+
+$('#menu a').hover(function(){
+    $(this).addClass('hover');
+}, function(){
+    $(this).removeClass('hover');
+});
+
+$('#menu a').tooltip({delay: 1000});
 
 // TODO: add the "hover" class to the menu items when you hover over them
 
